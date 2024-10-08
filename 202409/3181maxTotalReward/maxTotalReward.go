ee5e4d4acb4c7c61f552a5d9426f8b173fda493b
liuqianli <lq573320328@163.com>
2024-10-08 10:04:39 +0800

    学习

--- 202409/3181maxTotalReward/maxTotalReward.go
+++ 202409/3181maxTotalReward/maxTotalReward.go
@@ -1,34 +1,43 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-
+	fmt.Println(maxTotalReward([]int{1, 1, 3, 3}))
 }
 
 func maxTotalReward2(rewardValues []int) int {
 	n := len(rewardValues)
 	sort.Ints(rewardValues)
-	k := rewardValues[n-1]
+	k := rewardValues[n-1] * 2
 	// 定义 f[i][j] 表示能否从前 i 个数中得到总奖励 j。
-	f := make([][]int, n)
+	f := make([][]bool, n+1)
 	for i := range f {
-		f[i] = make([]int, k+1)
+		f[i] = make([]bool, k)
 	}
+	// 初始值 f[0][0]=true
+	f[0][0] = true
 
-	for i := 0; i <= k; i++ {
-		f[0][i] = min(rewardValues[0], i)
-	}
-	for i := 1; i < n; i++ {
-		for j := 0; j <= k; j++ {
+	for i := 1; i <= n; i++ {
+		v := rewardValues[i-1]
+		for j := 0; j < k; j++ {
 			f[i][j] = f[i-1][j]
-			if j >= rewardValues[i] {
-				f[i][j] = max(f[i][j], f[i-1][j-rewardValues[i]]+rewardValues[i])
+			if j >= v && j-v < v {
+				f[i][j] = f[i][j] || f[i-1][j-v]
 			}
 		}
 	}
 
-	return f[n-1][k]
+	// 答案为最大的满足 f[n][j]=true 的 j。
+	for j := k - 1; j >= 0; j-- {
+		if f[n][j] {
+			return j
+		}
+	}
+	return 0
 }
 
 // 会超时
