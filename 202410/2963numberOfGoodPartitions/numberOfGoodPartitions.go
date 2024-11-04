68457487a2e18a4eccd6480c401f433439762ed8
liuqianli <lq573320328@163.com>
2024-11-04 11:34:50 +0800

    学习

---
+++ 202410/2963numberOfGoodPartitions/numberOfGoodPartitions.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"math"
+	"slices"
+)
+
+func main() {
+
+}
+
+func numberOfGoodPartitions(nums []int) int {
+	m := make(map[int]pair)
+	for i, ch := range nums {
+		if p, ok := m[ch]; ok {
+			p.r = i
+			m[ch] = p
+		} else {
+			m[ch] = pair{l: i, r: i}
+		}
+	}
+	pairs := make([]pair, 0)
+	for _, p := range m {
+		pairs = append(pairs, p)
+	}
+	slices.SortFunc(pairs, func(a, b pair) int {
+		if a.l != b.l {
+			return a.l - b.l
+		}
+		return a.r - b.r
+	})
+	ans := 1
+	mod := int(math.Pow10(9)) + 7
+	mx := pairs[0].r
+	for i := 1; i < len(pairs); i++ {
+		if pairs[i].l > mx {
+			ans = ans * 2 % mod
+		}
+		mx = max(mx, pairs[i].r)
+	}
+	return ans
+}
+
+type pair struct {
+	l, r int
+}
