e013f009e987c890868359b79bc718a33964e685
liuqianli <lq573320328@163.com>
2024-11-21 10:08:54 +0800

    继续学习

--- 202411/2568minImpossibleOR/minImpossibleOR.go
+++ 202411/2568minImpossibleOR/minImpossibleOR.go
@@ -1,9 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	fmt.Println(minImpossibleOR([]int{2, 1}))
+	fmt.Println(minImpossibleOR([]int{5, 3, 2}))
 }
 
 func minImpossibleOR(nums []int) int {
+	exit := make(map[int]int)
+	for _, ch := range nums {
+		exit[ch] = 1
+	}
+	i := 0
+	for i <= 63 {
+		a := 1 << i
+		if exit[a] <= 0 {
+			return a
+		}
+		i++
+	}
 
+	return -1
 }
